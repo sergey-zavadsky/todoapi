59f8a843496266b9c5a0bb82ba20c93c95579ed2
Siarhei Zavadski <siarhei.zavadski@gmail.com>
2024-04-04 23:56:24 +0200

    swagger doc

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,44 @@ import express, { Request, Response, NextFunction } from 'express';
 import todosRoutes from './routes/todos';
 import { json } from 'body-parser';
 import cors from 'cors';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerUiConfig = {
+	definition: {
+		openapi: '3.1.0',
+		info: {
+			title: 'TODO list API',
+			version: '0.1.0',
+			description:
+				'This is a simple todo list api application which was made for educational purposes.',
+			license: {
+				name: 'MIT',
+				url: 'https://github.com/sergey-zavadsky/bucketlist.github.io/blob/master/LICENSE',
+			},
+			contact: {
+				name: 'TODO',
+				url: 'https://sergey-zavadsky.github.io/bucketlist.github.io/',
+				email: 'siarhei.zavadski@email.com',
+			},
+		},
+		servers: [
+			{
+				url: 'http://localhost:3000/api/v1/',
+			},
+		],
+	},
+
+	apis: ['src/routes/*.yaml'],
+};
 
 const app = express();
+const swaggerSpec = swaggerJsDoc(swaggerUiConfig);
+
 app.use(cors());
 app.use(json());
 app.use('/api/v1/todos', todosRoutes);
+app.use('/api/v1/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	res.status(400).json({ message: err.message });
--- src/controllers/createTodo/createTodo.ts
+++ src/controllers/createTodo/createTodo.ts
@@ -1,15 +1,19 @@
 import { RequestHandler } from 'express';
 import { db, dbCollection } from '../../db/dbConnection';
 import { Todo } from '../../models/todo';
-import { validateText } from '../validations';
+import { validateIsDone, validateText } from '../validations';
 
 export const createTodo: RequestHandler = async (req, res, next) => {
 	const dbCon = await db();
 	try {
 		const text = (req.body as { text: string }).text;
+		const isDone = (req.body as { isDone?: boolean }).isDone;
 
 		try {
 			validateText(text);
+			if (isDone !== undefined) {
+				validateIsDone(isDone);
+			}
 		} catch (error) {
 			const message = (error as Error).message;
 
@@ -17,7 +21,7 @@ export const createTodo: RequestHandler = async (req, res, next) => {
 		}
 
 		const date = new Date().toISOString();
-		const newTodo = new Todo(date, text);
+		const newTodo = new Todo(date, text, isDone);
 
 		await dbCon.сonnection?.collection(dbCollection).insertOne(newTodo);
 
